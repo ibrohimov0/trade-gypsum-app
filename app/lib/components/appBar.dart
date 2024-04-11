16c192faf398894b0272f6970b49050fd06fd5e9
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2024-04-11 19:55:41 +0500

    navbar

--- app/lib/components/appBar.dart
+++ app/lib/components/appBar.dart
@@ -3,26 +3,35 @@ import 'package:flutter/material.dart';
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
-  
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: const Text(
-      "BNBM",
-          style: TextStyle(
-            color: Color(0xffEE1B3F),
-            fontSize: 27,
-            fontWeight: FontWeight.bold,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "../assets/logo.png",
+            fit: BoxFit.contain,
+            height: 32,
           ),
-        ),
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("BNBM"),
+          ),
+        ],
+      ),
       backgroundColor: Color(0xff191825),
       elevation: 5.0,
       centerTitle: true,
-      leading: GestureDetector(
-        onTap: () {
-
-        }
-      ),
     );
   }
 }
+      // title: const Text(
+      //   "BNBM",
+      //   style: TextStyle(
+      //     color: Color(0xffEE1B3F),
+      //     fontSize: 27,
+      //     fontWeight: FontWeight.bold,
+      //   ),
+      // ),
