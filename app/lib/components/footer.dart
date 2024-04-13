f33b1386d28a79ee35febfb8ddb42381c9bcd744
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2024-04-13 16:21:19 +0500

    edits

--- app/lib/components/footer.dart
+++ app/lib/components/footer.dart
@@ -8,7 +8,7 @@ class Footer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(16),
-      child: Row(
+      child: Column(
         children: [
           Text(
             'Visit our social networks:', // Add a label for clarity
@@ -19,58 +19,60 @@ class Footer extends StatelessWidget {
             ),
           ),
           SizedBox(height: 15),
-          Column(children: [
-            ElevatedButton.icon(
-              onPressed: _launchURL,
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB(255, 215, 129, 230)),
-                  iconColor: MaterialStateProperty.all<Color>(Colors.white)),
-              icon: Icon(Icons.web),
-              label:
-                  Text('Open Website', style: TextStyle(color: Colors.white)),
-            ),
-            SizedBox(height: 8),
-            ElevatedButton.icon(
-              onPressed: _launchTelegram,
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB(255, 0x88, 0xCC, 0xFF)),
-                  iconColor: MaterialStateProperty.all<Color>(Colors.white)),
-              icon: Icon(Icons.telegram),
-              label:
-                  Text('Open Telegram', style: TextStyle(color: Colors.white)),
-            )
-          ]),
-          SizedBox(height: 8),
-          Column(
+          Row(
             children: [
-              ElevatedButton.icon(
-                onPressed: _launchInstagram,
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.black),
+              Column(children: [
+                ElevatedButton.icon(
+                  onPressed: _launchURL,
+                  style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all<Size>(Size(170,50)),
+                      backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 215, 129, 230)),
+                      iconColor: MaterialStateProperty.all<Color>(Colors.white)),
+                  icon: Icon(Icons.web),
+                  label: Text('Open Website',style: TextStyle(color: Colors.white)),
                 ),
-                icon: SizedBox(
-                  width: 20,
-                  height: 20,
-                  child: Image.asset(
-                    "assets/images/insta.png",
-                    fit: BoxFit.cover,
+                SizedBox(width: 8),
+                ElevatedButton.icon(
+                  onPressed: _launchTelegram,
+                  style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all<Size>(Size(170,50)),
+                      backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 0x88, 0xCC, 0xFF)),
+                      iconColor: MaterialStateProperty.all<Color>(Colors.white)),
+                  icon: Icon(Icons.telegram),
+                  label: Text('Open Telegram', style: TextStyle(color: Colors.white)),
+                )
+              ]),
+              SizedBox(height: 20),
+              Column(
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: _launchInstagram,
+                    style: ButtonStyle(
+                      backgroundColor:MaterialStateProperty.all<Color>(Colors.black),
+                      fixedSize: MaterialStateProperty.all<Size>(Size(170,50)),
+                    ),
+                    icon: SizedBox(
+                      width: 20,
+                      height: 20,
+                      child: Image.asset(
+                        "assets/images/insta.png",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    label: Text('Open Instagram', style: TextStyle(color: Colors.white)),
                   ),
-                ),
-                label: Text('Open Instagram',
-                    style: TextStyle(color: Colors.white)),
+                  SizedBox(width: 8,),
+                  ElevatedButton.icon(
+                    onPressed: _phoneCall,
+                    style: ButtonStyle(
+                        fixedSize: MaterialStateProperty.all<Size>(Size(170,50)),
+                        backgroundColor:MaterialStateProperty.all<Color>(Colors.green),
+                        iconColor:MaterialStateProperty.all<Color>(Colors.white)),
+                    icon: Icon(Icons.phone),
+                    label: Text("Call BNBM",style: TextStyle(color: Colors.white),),
+                  )
+                ],
               ),
-              ElevatedButton.icon(
-                onPressed: _phoneCall,
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-                  iconColor: MaterialStateProperty.all<Color>(Colors.white)
-                ),
-                icon: Icon(Icons.phone),
-                label: Text("Call BNBM", style: TextStyle(color: Colors.white),),
-              )
             ],
           ),
           SizedBox(height: 10),
@@ -118,9 +120,9 @@ class Footer extends StatelessWidget {
     }
   }
 
-  _phoneCall() async{
+  _phoneCall() async {
     final url = 'tel:+998910116631';
-    if(await canLaunch(url)) {
+    if (await canLaunch(url)) {
       await launch(url);
     } else {
       throw "Could not launch $url";
