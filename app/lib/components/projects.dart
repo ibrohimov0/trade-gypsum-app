94466b675f8d60e204fa5a735ee992bf9ae75706
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2024-04-30 19:13:30 +0500

    modal added

--- app/lib/components/projects.dart
+++ app/lib/components/projects.dart
@@ -38,41 +38,51 @@ class _ProjectsSectionState extends State<ProjectsSection> {
         SizedBox(height: 15),
         Column(
           children: projectsArr.map((project) {
-            return Container(
-              width: MediaQuery.of(context).size.width -50,
-              margin: EdgeInsets.only(top: 15,bottom: 15,left: 25),
-              decoration: BoxDecoration(
-                color: project.boxColor,
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Image.asset(project.iconPath),
+            return GestureDetector(
+              onTap: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: ((context) {
+                    return Container();
+                  }),
+                );
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width - 50,
+                margin: EdgeInsets.only(top: 15, bottom: 15, left: 25),
+                decoration: BoxDecoration(
+                  color: project.boxColor,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Image.asset(project.iconPath),
+                      ),
                     ),
-                  ),
-                  Text(
-                    project.name,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white,
-                      fontSize: 23,
+                    Text(
+                      project.name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                        fontSize: 23,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 20,),
-                  Text(
-                    project.text,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white70,
-                      fontSize: 15,
+                    SizedBox(height: 20,),
+                    Text(
+                      project.text,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 15,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 20,)
-                ],
+                    SizedBox(height: 20,)
+                  ],
+                ),
               ),
             );
           }).toList(),
