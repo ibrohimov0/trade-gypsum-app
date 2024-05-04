764822670dd3ac9b1730c35bb06de9ff26bf5de3
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2024-05-04 20:02:22 +0500

    ok

--- app/lib/models/projects_model.dart
+++ app/lib/models/projects_model.dart
@@ -24,8 +24,8 @@ class ProjectModel {
         iconPath: "assets/images/gypsum(1).png",
         name: "Sinogips", 
         text: "SINOGIPS 9.5 - гипсокартон патолочный. Размер:(1200*2500*9.5)",
-        android3dPath: "assets/3d-models/yawil.gltf",
-        ios3dPath: "assests/3d-models/Astronaut.usdz"
+        android3dPath: "assets/3d-models/gypsum(1).glb",
+        ios3dPath: "assests/3d-models/gypsum(1).usdz"
       )
     );
     projectsArr.add(
@@ -34,8 +34,8 @@ class ProjectModel {
         iconPath: "assets/images/gypsum(2).png",
         name: "Sinogips", 
         text: "SINOGIPS 9.5 -гипсокартон патолочный влагостойкий. Размер : (1200*2500*9,5)",
-        android3dPath: "assets/3d-models/kok.gltf",
-        ios3dPath: "assests/3d-models/Astronaut.usdz"
+        android3dPath: "assets/3d-models/gypsum(2).glb",
+        ios3dPath: "assests/3d-models/gypsum(2).usdz"
       )
     );
     projectsArr.add(
@@ -44,8 +44,8 @@ class ProjectModel {
         iconPath: "assets/images/gypsum(3).png",
         name: "Sinogips", 
         text: "SINOGIPS 12.5 -гипсокартон стеновой влагостойкий. Размер : (1200*2500*12,5)",
-        android3dPath: "assets/3d-models/ikkitali.gltf",
-        ios3dPath: "assests/3d-models/Astronaut.usdz"
+        android3dPath: "assets/3d-models/gypsum(3).glb",
+        ios3dPath: "assests/3d-models/gypsum(3).usdz"
       )
     );
     return projectsArr;
